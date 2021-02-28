c4caea693395b1525fbb78c9611437f191625c5f
michael beamer <beamermichael@gmail.com>
2021-02-28 15:54:23 +0100

    walk rendering with correct data - added

--- js/main.js
+++ js/main.js
@@ -34,21 +34,23 @@ class App {
     const distance = distanceSlider.value;
     const duration = durationSlider.value;
     const walkType = walkTypeSelect.value;
+    let walk;
 
     //check if its a date or casual
     if (walkType === "casual") {
       const ambience = ambienceSlider.value;
-      const walk = new Casual(distance, duration, ambience);
-      this.#walks.push(walk);
-      console.log(this.#walks);
+      walk = new Casual(distance, duration, ambience);
     }
 
     if (walkType === "romantic") {
       const romanticLvl = romanticSlider.value;
-      const walk = new Romantic(distance, duration, romanticLvl);
-      this.#walks.push(walk);
-      console.log(this.#walks);
+      walk = new Romantic(distance, duration, romanticLvl);
     }
+    this.#walks.push(walk);
+    console.log(this.#walks);
+
+    // Renders walk in list - helper function
+    this._renderWalk(walk);
   }
 
   _getLocation() {
@@ -96,6 +98,42 @@ class App {
       .openPopup();
 
     this._showForm();
+  }
+
+  _renderWalk(walk) {
+    let html = `    
+    <li class="walk ${walk.type}--casual" data-id="22323223">
+      <h2 class="walk__title">${walk.type} walk on February 22</h2>
+      <div class="walk__details">
+        <span class="walk__icon">👣</span>
+        <span class="walk__value">${walk.distance}</span>
+        <span class="walk__unit">km</span>
+      </div>
+      <div class="walk__details">
+        <span class="walk__icon">⏱</span>
+        <span class="walk__value">${walk.duration}</span>
+        <span class="walk__unit">min</span>
+      </div>`;
+
+    if (walk.type === "casual") {
+      html += `
+        <div class="walk__details">
+            <span class="walk__icon">Ambience Level</span>
+            <span class="walk__value">${"⭐️ ".repeat(walk.ambience)}</span>
+        </div>
+    </li>`;
+    }
+
+    if (walk.type === "romantic") {
+      html += `    
+        <div class="walk__details">
+            <span class="walk__icon">Romantic Level</span>
+            <span class="walk__value">${"❤️ ".repeat(walk.romanticLvl)}</span>
+        </div>
+    </li>`;
+    }
+
+    formContainer.insertAdjacentHTML("beforeend", html);
   }
 
   _showForm() {
